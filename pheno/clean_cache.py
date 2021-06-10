dbfa8f583e399c86aeb867bd3a12201b28c528f2
Anders Pitman <tapitman11@gmail.com>
2021-06-10 18:43:31 +0000

    Update cleaner script to remove invalid entries

--- pheno/clean_cache.py
+++ pheno/clean_cache.py
@@ -8,4 +8,10 @@ for root, dirs, files in os.walk('./cache'):
         if size == 0:
             print(path)
             os.remove(path)
+        else:
+            with open(path, 'r') as f:
+                data = f.read(10)
+                if not data.startswith('Rank'):
+                    print(path)
+                    os.remove(path)
 
